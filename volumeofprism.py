212cba7e195e853533f807e5af0d127c51fdf426
BHARAT KUMAR PRAJAPAT <123304396+bharatprajapat014@users.noreply.github.com>
2023-02-15 10:55:56 +0530

    Create volumeofprism.py

---
+++ volumeofprism.py
@@ -0,0 +1,6 @@
+base = float(input("Enter the area of the base of the prism: "))
+height = float(input("Enter the height of the prism: "))
+
+volume = base * height
+
+print("The volume of the prism is:", volume)
